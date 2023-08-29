6951513b2306a7e16b1c9fb45eb9b098eaf312e8
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-08-29 11:36:15 +0900

    bottomNav + db작성

--- app/src/main/java/com/example/applemarket/MainActivity.kt
+++ app/src/main/java/com/example/applemarket/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.applemarket
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var townFragment: TownFragment
+    private lateinit var aroundFragment: AroundFragment
+    private lateinit var chatFragment: ChatFragment
+    private lateinit var settingFragment: SettingFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        bottomNav()
+
+        //아이템 클릭? 시 선택된 컬러
+        binding.navMain.itemActiveIndicatorColor = null
+
+
+    }
+
+    private fun bottomNav() {
+
+        homeFragment = HomeFragment()
+        townFragment = TownFragment()
+        aroundFragment = AroundFragment()
+        chatFragment = ChatFragment()
+        settingFragment = SettingFragment()
+
+        binding.navMain.run {
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.home -> changeFragment(homeFragment)
+                    R.id.town -> changeFragment(townFragment)
+                    R.id.around -> changeFragment(aroundFragment)
+                    R.id.chat -> changeFragment(chatFragment)
+                    R.id.setting -> changeFragment(settingFragment)
+                }
+                true
+            }
+            selectedItemId = R.id.home
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.frameLayout.id, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/applemarket/TownFragment.kt
@@ -0,0 +1,20 @@
+package com.example.applemarket
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TownFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_town, container, false)
+    }
+
+
+}
