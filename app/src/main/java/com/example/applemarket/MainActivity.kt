04d80776cf088c1cd815e271cc4bb29e59c95263
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-09-02 01:09:40 +0900

    backPressed button 기능 구현

--- app/src/main/java/com/example/applemarket/MainActivity.kt
+++ app/src/main/java/com/example/applemarket/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.applemarket
 
+import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.applemarket.Main.AroundFragment
@@ -26,6 +32,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         bottomNav()
+
+        //기존 backPressed가 description되어 콜백인스턴스 붙여주기
+        this.onBackPressedDispatcher.addCallback(this, callback)
+
 
         //아이템 클릭? 시 선택된 컬러
         binding.navMain.itemActiveIndicatorColor = null
@@ -65,5 +75,29 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(binding.frameLayout.id, fragment)
             .commit()
+    }
+
+    //콜백 인스턴스 생성
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            // 뒤로 버튼 이벤트 처리
+            var builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("종료")
+            builder.setMessage("정말 종료하실꺼예요!??!?!?!?")
+            builder.setIcon(R.drawable.ic_chat)
+
+            val listener = DialogInterface.OnClickListener { dialog, which ->
+                when (which) {
+
+                    DialogInterface.BUTTON_POSITIVE ->
+                        finish()
+                }
+            }
+            builder.setPositiveButton("종료", listener)
+            builder.setNegativeButton("취소", listener)
+
+            builder.show()
+
+        }
     }
 }
