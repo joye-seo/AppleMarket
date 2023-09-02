54475c8e1f06e0826c2717f21d18fd616f0fda9b
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-09-03 03:02:50 +0900

    디테일 하트 클릭 시 이미지 변경

--- app/src/main/java/com/example/applemarket/DetailHomeActivity.kt
+++ app/src/main/java/com/example/applemarket/DetailHomeActivity.kt
@@ -5,8 +5,10 @@ import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import com.example.applemarket.data.Goods
 import com.example.applemarket.databinding.ActivityDetailHomeBinding
+import com.google.android.material.snackbar.Snackbar
 
 class DetailHomeActivity : AppCompatActivity() {
+    private var isClicked = false
     private lateinit var goods: Goods
 
     private lateinit var binding: ActivityDetailHomeBinding
@@ -53,6 +55,29 @@ class DetailHomeActivity : AppCompatActivity() {
             }
             popupMenu.show()
         }
+
+        btnHeart.setOnClickListener {
+
+            if (isClicked) {
+                btnHeart.setImageResource(R.drawable.ic_home_heart)
+                Snackbar.make(
+                    it, // 현재 뷰 (하트 버튼이 클릭된 뷰)
+                    "관심 목록에 취소되었습니다.", // 스낵바에 표시할 메시지
+                    Snackbar.LENGTH_SHORT // 스낵바를 표시할 시간
+                ).show()
+                isClicked = false
+            } else {
+                btnHeart.setImageResource(R.drawable.ic_home_heart_click)
+                // 스낵바를 생성하고 표시
+                Snackbar.make(
+                    it, // 현재 뷰 (하트 버튼이 클릭된 뷰)
+                    "관심 목록에 추가되었습니다.", // 스낵바에 표시할 메시지
+                    Snackbar.LENGTH_SHORT // 스낵바를 표시할 시간
+                ).show()
+                isClicked = true
+            }
+        }
+
 
     }
 
